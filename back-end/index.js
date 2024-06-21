a16e9e3bccaa67ce615a1a1784a7157ae79578e8
Michelle Johanna Ordonez <mordonezcampos@meta.com>
2024-06-21 00:40:19 -0700

    can add card through insomnia at the moment, working on displaying when selected board

--- back-end/index.js
+++ back-end/index.js
@@ -5,14 +5,14 @@ const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 
+/*one im working on */
 app.use(express.json());
 app.use(cors());
 
-app.get("/boards", async (req, res) => {
+app.get('/boards', async (req, res) => {
     const boards = await prisma.board.findMany()
-    res.status(200).json(boards);
+    res.json(boards);
 });
-
 // app.get("/boards", async (req, res) => {
 //     let query = 'SELECT * FROM boards'
 //     const queryParams = []
@@ -38,7 +38,7 @@ app.get("/boards", async (req, res) => {
 //     // res.status(200).json(boards);
 // });
 
-app.get("/boards/:id", async (req, res) => {
+app.get('/boards/:id', async (req, res) => {
         const { id } = req.params
         const boards = await prisma.board.findUnique(
         {
@@ -47,14 +47,13 @@ app.get("/boards/:id", async (req, res) => {
         res.status(200).json(boards);
     });
 
-app.post("/boards", async (req, res) => {
+app.post('/boards', async (req, res) => {
     const { img, title, author, description, category } = req.body;
     const newBoard = await prisma.board.create({
         data: {
             img,
             title,
             author,
-            // category,
             description,
             category
         }
@@ -69,31 +68,27 @@ app.delete('/boards/:id', async (req, res) => {
         where: { id: parseInt(id) }
     })
     res.json(deletedBoard)
-})
+});
+
+app.get('/cards', async (req, res) => {
+    const cards = await prisma.card.findMany();
+    res.json(cards)
+});
+
+app.post('/boards/cards', async (req, res) => {
+    const {id, message, img, author, boardId} = req.body;
+    const newCard = await prisma.card.create({
+        data: {
+            id,
+            message,
+            img,
+            author,
+            boardId
+        }
+    })
+    res.status(201).json(newCard)
+});
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running on http://localhost;${PORT}`)
 });
-
-// app.get("/books:id", async (req, res) => {
-//     const id = parseInt(req.params.id)
-//     const book = await prisma.book.findUnique()
-// })
-
-// app.post("/books", async (req, res) => {
-//     const {title, author} = req.body;
-//     const newBook = await prisma.book.create(
-//         data: {
-//             title,
-//             author
-//         }
-//         }
-// })
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`)
-//   })
-
-// app.get('/', (req, res) => {
-//     res.send(App())
-// })
